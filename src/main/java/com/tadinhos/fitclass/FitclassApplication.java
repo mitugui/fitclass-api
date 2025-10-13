d8d92b509005b4bf600f99260113c33d449bd1f9
mitugui <cristian.mitugui04@gmail.com>
2025-10-13 14:04:02 -0300

    chore: initialize SpringBoot project with base setup - Bootstrapped project using Spring Boot 3.5.5 with Java 17. - Added core dependencies:   - spring-boot-starter-web → REST API development.   - spring-boot-starter-data-jpa → persistence layer with JPA/Hibernate.   - spring-boot-starter-validation → request validation (Jakarta Validation).   - spring-boot-devtools → hot reload during development.   - lombok → boilerplate reduction with annotations.   - flyway-core & flyway-mysql → database migrations management.   - mysql-connector-j → runtime MySQL driver.

---
+++ src/main/java/com/tadinhos/fitclass/FitclassApplication.java
@@ -0,0 +1,13 @@
+package com.tadinhos.fitclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tadinhos/fitclass/FitclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tadinhos.fitclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
