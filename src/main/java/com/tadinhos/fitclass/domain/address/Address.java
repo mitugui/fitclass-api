547f47ee625700246f984f760d50b789fb49a449
mitugui <cristian.mitugui04@gmail.com>
2025-10-16 15:08:29 -0300

    feat: add User model and update Address relationship - Added User model with JPA and Boilerplate annotations. - Updated Address to include one-to-many relationship with User.

--- src/main/java/com/tadinhos/fitclass/domain/address/Address.java
+++ src/main/java/com/tadinhos/fitclass/domain/address/Address.java
@@ -1,7 +1,13 @@
 package com.tadinhos.fitclass.domain.address;
 
+import com.tadinhos.fitclass.domain.user.User;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity(name = "Address")
 @Table(name = "addresses")
@@ -17,4 +23,7 @@ public class Address {
     private String address;
     private String cep;
     private String number;
+
+    @OneToMany(mappedBy = "address", fetch = FetchType.LAZY)
+    private List<User> users;
 }
---
+++ src/main/java/com/tadinhos/fitclass/domain/user/User.java
@@ -0,0 +1,35 @@
+package com.tadinhos.fitclass.domain.user;
+
+import com.tadinhos.fitclass.domain.address.Address;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity(name = "User")
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String cpf;
+    private char gender;
+    private LocalDate birthDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
+}
