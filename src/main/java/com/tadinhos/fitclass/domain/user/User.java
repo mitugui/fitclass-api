08ed12809347443bc59b40045d07e1c0ab6281fb
mitugui <cristian.mitugui04@gmail.com>
2025-10-22 09:46:36 -0300

    feat: add dedicated User constructor to support subclass initialization without including id

--- src/main/java/com/tadinhos/fitclass/domain/user/User.java
+++ src/main/java/com/tadinhos/fitclass/domain/user/User.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 @Entity(name = "User")
 @Table(name = "users")
@@ -28,9 +27,21 @@ public class User {
     private String phoneNumber;
     private String cpf;
     private char gender;
+    @Column(name = "birthdate")
     private LocalDate birthDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "address_id")
     private Address address;
+
+    public User(String name, String email, String password, String phoneNumber, String cpf, char gender, LocalDate birthDate, Address address) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.cpf = cpf;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.address = address;
+    }
 }
