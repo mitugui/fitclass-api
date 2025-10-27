d2a2cebe87c8a785596fa023a39a3dff4c8997b2
mitugui <cristian.mitugui04@gmail.com>
2025-10-27 15:46:01 -0300

    refactor: simplify GymOwner creation by directly instantiating Address and using cascade persistence

--- src/main/java/com/tadinhos/fitclass/domain/gymowner/GymOwnerService.java
+++ src/main/java/com/tadinhos/fitclass/domain/gymowner/GymOwnerService.java
@@ -1,7 +1,6 @@
 package com.tadinhos.fitclass.domain.gymowner;
 
 import com.tadinhos.fitclass.domain.address.Address;
-import com.tadinhos.fitclass.domain.address.AddressRepository;
 import com.tadinhos.fitclass.domain.address.dto.DataAddressCreate;
 import com.tadinhos.fitclass.domain.gymowner.dto.DataGymOwnerCreate;
 import com.tadinhos.fitclass.domain.gymowner.dto.DataGymOwnerCreateRequest;
@@ -15,31 +14,16 @@ import org.springframework.web.server.ResponseStatusException;
 public class GymOwnerService {
     @Autowired
     private GymOwnerRepository gymOwnerRepository;
-    @Autowired
-    private AddressRepository addressRepository;
 
     public DataGymOwnerCreated create(DataGymOwnerCreateRequest request) {
-        var address = createAddress(request);
-
-        var gymOwnerData = new DataGymOwnerCreate(request, address);
+        var address = new Address(new DataAddressCreate(request));
+        var gymOwner = new GymOwner(new DataGymOwnerCreate(request, address));
 
         try {
-            return new DataGymOwnerCreated(gymOwnerRepository.save(new GymOwner(gymOwnerData)));
+            return new DataGymOwnerCreated(gymOwnerRepository.save(gymOwner));
         } catch (RuntimeException e) {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST, "Erro ao salvar GymOwner", e
-            );
-        }
-    }
-
-    private Address createAddress(DataGymOwnerCreateRequest request) {
-        var dataAddressCreate = new DataAddressCreate(request);
-
-        try {
-            return addressRepository.save(new Address(dataAddressCreate));
-        } catch (RuntimeException e) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "Erro ao salvar endereço", e
             );
         }
     }
