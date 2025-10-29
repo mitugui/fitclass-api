b7988fae122e028e59c3f058f9abbae0c223cc9b
yoshimidevz <yoshimidev1@gmail.com>
2025-10-29 10:41:20 -0300

    feat: add transactional annotation in post route

--- src/main/java/com/tadinhos/fitclass/controller/GymOwnerController.java
+++ src/main/java/com/tadinhos/fitclass/controller/GymOwnerController.java
@@ -3,6 +3,9 @@ package com.tadinhos.fitclass.controller;
 import com.tadinhos.fitclass.domain.gymowner.GymOwnerService;
 import com.tadinhos.fitclass.domain.gymowner.dto.DataGymOwnerCreateRequest;
 import com.tadinhos.fitclass.domain.gymowner.dto.DataGymOwnerCreated;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,7 @@ public class GymOwnerController {
     private GymOwnerService gymOwnerService;
 
     @PostMapping
+    @Transactional
     public ResponseEntity<DataGymOwnerCreated> save (@RequestBody DataGymOwnerCreateRequest dataGymOwnerCreateRequest) {
         var gymOwnerCreated = gymOwnerService.create(dataGymOwnerCreateRequest);
 
