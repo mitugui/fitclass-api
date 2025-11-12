a5f2217fdca88fc27d66dab61ce17f6fee97be0c
mitugui <cristian.mitugui04@gmail.com>
2025-11-12 09:33:15 -0300

    refactor: change findByEmail return type from just UserDetails to Optional<UserDetails>

--- src/main/java/com/tadinhos/fitclass/domain/user/UserRepository.java
+++ src/main/java/com/tadinhos/fitclass/domain/user/UserRepository.java
@@ -3,6 +3,8 @@ package com.tadinhos.fitclass.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByEmail(String email);
+    Optional<UserDetails> findByEmail(String email);
 }
