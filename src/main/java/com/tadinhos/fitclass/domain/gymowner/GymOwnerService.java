8144d74258b9e08b0760c3864818bd3d7147ddf2
mitugui <cristian.mitugui04@gmail.com>
2025-11-12 09:39:05 -0300

    feat: implement GymOwner password encoding with PasswordEncoder

--- src/main/java/com/tadinhos/fitclass/domain/gymowner/GymOwnerService.java
+++ src/main/java/com/tadinhos/fitclass/domain/gymowner/GymOwnerService.java
@@ -7,6 +7,7 @@ import com.tadinhos.fitclass.domain.gymowner.dto.DataGymOwnerCreateRequest;
 import com.tadinhos.fitclass.domain.gymowner.dto.DataGymOwnerCreated;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,9 +16,26 @@ public class GymOwnerService {
     @Autowired
     private GymOwnerRepository gymOwnerRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public DataGymOwnerCreated create(DataGymOwnerCreateRequest request) {
         var address = new Address(new DataAddressCreate(request));
-        var gymOwner = new GymOwner(new DataGymOwnerCreate(request, address));
+
+        var encodedPassword = passwordEncoder.encode(request.password());
+
+        var dataCreate = new DataGymOwnerCreate(
+                request.name(),
+                request.email(),
+                encodedPassword,
+                request.phoneNumber(),
+                request.cpf(),
+                request.gender().charAt(0),
+                request.birthDate(),
+                address
+        );
+
+        var gymOwner = new GymOwner(dataCreate);
 
         try {
             return new DataGymOwnerCreated(gymOwnerRepository.save(gymOwner));
